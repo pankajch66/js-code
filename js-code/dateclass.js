8036b90bbc7669d7eeea7c1d08b8fc87e103a7a1
pankajch66 <142115868+pankajch66@users.noreply.github.com>
2023-08-27 14:53:02 +0000

    array turorial important

---
+++ js-code/dateclass.js
@@ -0,0 +1,24 @@
+// let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toString());
+// console.log(myDate.getDay());
+// console.log(String(myDate.getDay()));
+
+// const weekDay = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday'];
+// const d = new Date();
+// let day = weekDay[d.getDay()];
+// console.log(day);
+
+// let a = ['pankaj'];
+// console.log(a);
+
+const arr = [1,2,3,4,5];
+// console.log(arr);
+// console.log(arr.slice(1,4));
+console.log(arr);
+console.log(arr.splice(1,4));
+console.log(arr);
