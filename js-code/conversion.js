3340e180d4c68ddd9e684387d7f9589d6a3294ee
pankajch66 <142115868+pankajch66@users.noreply.github.com>
2023-08-27 10:33:22 +0000

    data typeof summary

--- js-code/conversion.js
+++ js-code/conversion.js
@@ -2,3 +2,17 @@ let names1 = 123;
 console.log(typeof names1);
 let names2 = String(names1);
 console.log(typeof names2);
+
+console.log(2+2+'2');
+console.log(undefined >= 0);
+
+/*
+if checked type of then it results -
+undefined = undefined
+null = object
+boolean = boolean
+number = number
+string = string
+object = object
+
+*/
