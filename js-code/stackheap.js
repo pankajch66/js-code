32e338a5b43872302f715c55f02cba1e8e215b4d
pankajch66 <142115868+pankajch66@users.noreply.github.com>
2023-08-27 12:43:20 +0000

    all changes done

--- js-code/stackheap.js
+++ js-code/stackheap.js
@@ -1,8 +1,17 @@
 //stack is primitive menory means it gives a copy of its value
 //heap is nonprimitive means it gives original copy of its value
 
-let userOne = {
-    email:"pankaj@gmil.com",
-    phone : 698574
-}
-console.log(userOne);
+// let userOne = {
+//     email:"pankaj@gmil.com",
+//     phone : 698574
+// }
+// console.log(userOne);
+
+// let myName = "pankaj";
+// console.log(myName.charAt(0));
+// console.log(myName.indexOf('n'));
+
+// const url = "https://hitesh.com/hitesh%3choudhary";
+// console.log(url.replace('%3','-'));
+// console.log(url.includes('hitesh'));
+// console.log(url.split(' '));
