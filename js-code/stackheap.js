8729359f01e0c9829249e334a8a4c69480e3652e
pankajch66 <142115868+pankajch66@users.noreply.github.com>
2023-08-27 12:08:50 +0000

    memory allocation and how it work

---
+++ js-code/stackheap.js
@@ -0,0 +1,8 @@
+//stack is primitive menory means it gives a copy of its value
+//heap is nonprimitive means it gives original copy of its value
+
+let userOne = {
+    email:"pankaj@gmil.com",
+    phone : 698574
+}
+console.log(userOne);
