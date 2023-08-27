d967db80c5053189715d9d2d991861a65c390b1f
pankajch66 <142115868+pankajch66@users.noreply.github.com>
2023-08-27 14:04:24 +0000

    math related rules and example

---
+++ js-code/maths.js
@@ -0,0 +1,21 @@
+// const score = 400.69;
+// console.log(score.toFixed(2));
+// console.log(score.toPrecision(4));//used to round of number after dot but number should be fixed that should be put.
+
+// const scoreOne = 1000000;
+// console.log(scoreOne.toLocaleString());//this will output in us standard
+// console.log(scoreOne.toLocaleString('en-IN'));//this will output in indian standard
+
+//maths library
+
+// console.log(Math);
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(8,6,9,2,4,0,1,-1));
+// console.log(Math.max(8,6,9,2,4,0,1,-1));
+console.log(Math.random());
+console.log((Math.random()*6) + 1);
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()*(max-min + 1))+ min);
